a668cdcb33f6734cb0fde07ecf73fd9535f2d68c
rhn <gihu.rhn@porcupinefactory.org>
2020-01-03 21:05:13 +0000

    Update remote script to Python3

--- remote_script.py
+++ remote_script.py
@@ -4,12 +4,12 @@ import os.path
 import struct
 import sys
 
-(remote_size, blocksize, filename_len, hashname_len) = struct.unpack('<QQQQ', sys.stdin.read(8+8+8+8))
+(remote_size, blocksize, filename_len, hashname_len) = struct.unpack('<QQQQ', sys.stdin.buffer.read(8+8+8+8))
 filename = sys.stdin.read(filename_len)
 hashname = sys.stdin.read(hashname_len)
 
-sanity_hash = hashlib.new(hashname, filename).digest()
-sys.stdout.write(sanity_hash)
+sanity_hash = hashlib.new(hashname, filename.encode('utf-8')).digest()
+sys.stdout.buffer.write(sanity_hash)
 
 f = open(filename, 'rb')
 try:
@@ -17,7 +17,7 @@ try:
     size = f.tell()
 finally:
     f.close()
-sys.stdout.write(struct.pack('<Q', size))
+sys.stdout.buffer.write(struct.pack('<Q', size))
 
 sys.stdout.flush()
 
@@ -40,12 +40,12 @@ try:
             break
         hash_total.update(block)
         digest = hashlib.new(hashname, block).digest()
-        sys.stdout.write('d')
-        sys.stdout.write(digest)
+        sys.stdout.buffer.write(b'd')
+        sys.stdout.buffer.write(digest)
         sys.stdout.flush()
         if sys.stdin.read(4) == 'send':
-            sys.stdout.write('b')
-            sys.stdout.write(block)
+            sys.stdout.buffer.write(b'b')
+            sys.stdout.buffer.write(block)
             sys.stdout.flush()
         readremain -= rblocksize
         if readremain == 0:
@@ -53,32 +53,7 @@ try:
 finally:
     f.close()
 
-sys.stdout.write('t')
-sys.stdout.write(hash_total.digest())
+sys.stdout.buffer.write(b't')
+sys.stdout.buffer.write(hash_total.digest())
 sys.stdout.flush()
 
-end
-'''
-    while True:
-        position_s = sys.stdin.read(8)
-        if len(position_s) == 0:
-            break
-        (position,) = struct.unpack('<Q', position_s)
-        block = sys.stdin.read(blocksize)
-        f.seek(position)
-        f.write(block)
-    readremain = size
-    rblocksize = blocksize
-    hash_total = hashlib.new(hashname)
-    f.seek(0)
-    while True:
-        if readremain <= blocksize:
-            rblocksize = readremain
-        block = f.read(rblocksize)
-        if len(block) == 0:
-            break
-        hash_total.update(block)
-        readremain -= rblocksize
-        if readremain == 0:
-	    break
-sys.stdout.write(hash_total.digest())'''
