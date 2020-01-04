f7c4db98448dbf5d8ad802a354fa286ce3049d10
rhn <gihu.rhn@porcupinefactory.org>
2020-01-04 13:40:27 +0000

    Fix file headers

--- remote_script.py
+++ remote_script.py
@@ -1,3 +1,24 @@
+#!/usr/bin/env python3
+
+# Copyright (C) 2012-220
+#
+# * Volker Diels-Grabsch <v@njh.eu>
+# * art0int <zvn_mail@mail.ru>
+# * Matthew Fearnley (matthew.w.fearnley@gmail.com)
+# * rhn <gihu.rhn@porcupinefactory.org>
+#
+# Permission to use, copy, modify, and/or distribute this software for any
+# purpose with or without fee is hereby granted, provided that the above
+# copyright notice and this permission notice appear in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+# OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
 import hashlib
 import os
 import os.path
