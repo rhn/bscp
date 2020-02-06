19528906c4e7fbab099cd2916955bc2e2055669b
rhn <gihu.rhn@porcupinefactory.org>
2020-02-06 11:56:08 +0000

    Faster no-change transfer
    The remote script doesn't wait for confirmation after each read block.

--- remote_script.py
+++ remote_script.py
@@ -24,6 +24,8 @@ import os
 import os.path
 import struct
 import sys
+import threading
+
 
 (remote_size, blocksize, offset_blocks, filename_len, hashname_len) = struct.unpack('<QQQQQ', sys.stdin.buffer.read(8+8+8+8+8))
 filename = sys.stdin.buffer.read(filename_len)
@@ -45,37 +47,61 @@ sys.stdout.flush()
 if sys.stdin.buffer.read(2) != b'go':
     sys.exit()
 
-hash_total = hashlib.new(hashname)
+hashes = []
 
-f = open(filename, 'rb')
-try:
-    f.seek(0, os.SEEK_END)
-    readremain = size - offset_blocks * blocksize
-    rblocksize = blocksize
-    f.seek(offset_blocks * blocksize)
-    while True:
-        if readremain <= blocksize:
-            rblocksize = readremain
-        block = f.read(rblocksize)
-        if len(block) != rblocksize:
-            break
-        hash_total.update(block)
-        digest = hashlib.new(hashname, block).digest()
-        sys.stdout.buffer.write(b'd')
-        sys.stdout.buffer.write(digest)
-        sys.stdout.flush()
-        if sys.stdin.buffer.read(4) == b'send':
-            sys.stdout.buffer.write(b'b')
-            sys.stdout.buffer.write(block)
-            sys.stdout.buffer.write(hash_total.digest())
-            sys.stdout.flush()
-        readremain -= rblocksize
-        if readremain == 0:
-            break
-finally:
-    f.close()
+def send_digests():
+    hash_total = hashlib.new(hashname)
+    with open(filename, 'rb') as f:
+        f.seek(0, os.SEEK_END)
+        readremain = size - offset_blocks * blocksize
+        rblocksize = blocksize
+        f.seek(offset_blocks * blocksize)
+        readremain = size
+        rblocksize = blocksize
+        while True:
+            if readremain <= blocksize:
+                rblocksize = readremain
+            block = f.read(rblocksize)
+            if len(block) != rblocksize:
+                break # something went wrong
+            hash_total.update(block)
+            hashes.append(hash_total.digest())
+            digest = hashlib.new(hashname, block).digest()
+            with send_packet:
+                sys.stdout.buffer.write(b'd')
+                sys.stdout.buffer.write(digest)
+            readremain -= rblocksize
+            if readremain == 0:
+                break
+    with send_packet:
+        sys.stdout.buffer.write(b't')
+        sys.stdout.buffer.write(hash_total.digest())
+        sys.stdout.buffer.flush()
 
-sys.stdout.buffer.write(b't')
-sys.stdout.buffer.write(hash_total.digest())
-sys.stdout.flush()
+
+def handle_data():
+    with open(filename, 'rb') as f:
+        while True:
+            pkt = sys.stdin.buffer.read(4)
+            if pkt == b'send':
+                (block_idx,) = struct.unpack('<Q', sys.stdin.buffer.read(8))
+                f.seek(block_idx * blocksize)
+                read_size = min(remote_size - block_idx * blocksize, blocksize)
+                block = f.read(read_size)
+                with send_packet:
+                    sys.stdout.buffer.write(b'b')
+                    sys.stdout.buffer.write(block)
+                    sys.stdout.buffer.write(hashes[block_idx])
+                sys.stdout.buffer.flush()
+            elif pkt == b'':
+                break
+            else:
+                raise RuntimeError("Bad packet received: {}", pkt)
+
+send_packet = threading.Lock()
+
+datas = threading.Thread(target=handle_data)
+datas.start()
+send_digests()
+datas.join()
 
